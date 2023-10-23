f3a4e72f1a5724a7b282867f8f88a57722745e8b
Tonmoy <shahariartonmoy17@gmail.com>
2023-10-23 09:55:26 +0600

    Post api created

--- index.js
+++ index.js
@@ -33,16 +33,42 @@ const client = new MongoClient(uri, {
   }
 });
 
+const database = client.db("brandsDB");
+const brandProductsCollection = database.collection("products");
+// const ferrariCollection = database.collection("ferrari");
+// const lamborghiniCollection = database.collection("lamborghini");
+// const bugattiCollection = database.collection("bugatti");
+// const mclarenCollection = database.collection("mclaren");
+// const porscheCollection = database.collection("porsche");
+// const koenigseggCollection = database.collection("koenigsegg");
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.get('/products', async (req, res) =>{
+        const cursor = brandProductsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      })
+
+    //create data or insert a data to database
+    app.post("/products", async (req, res) => {
+        console.log(req.body);
+        const newProduct = req.body;
+        const result = await brandProductsCollection.insertOne(newProduct);
+        res.send(result);
+        console.log(result);
+        
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
